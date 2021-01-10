d91bd76b22ca4568c972c8ab3b0e08bfdbf22607
kor-Randy <randy_95@naver.com>
2021-01-10 23:39:32 +0900

    List-1 -> Range, RandomAccess, ForEach

--- SwiftUI_Practice/ListView.swift
+++ SwiftUI_Practice/ListView.swift
@@ -35,6 +35,32 @@ struct ListView: View {
                 Text("9")
                 Text("10")
             }
+            //Range<Int>
+            List(1..<10){
+                //... 오류남
+                Text("\($0)")
+            }
+            //RandomAccessCollection
+            //방법 1
+            //id 매개변수에는 Hashable 프로토콜을 준수하는 프로퍼티를 지정 가능
+            //데이터 타입 자체가 Hashable을 준수하면 아래처럼 self 사용 가능
+            //사용자 정의 커스텀 타입에서는 Hashable 프로토콜을 채택해야 함
+            //방법 2
+            //데이터 타입 자체에 Swift5.1에서 추가된
+            //identifiable 프로토콜을 채택하는 방법
+            //방법 2를 사용한다면 id를 제공하지 않아도 됨
+            List(["A","B","C","D","E","F"], id: \.self){
+                Text("\($0)")
+            }
+            
+            //ForEach
+            //Range 방법과 비슷함
+            List{
+                ForEach(0..<10){
+                    Text("\($0)")
+                }
+            }
+            
         }
     }
 }
